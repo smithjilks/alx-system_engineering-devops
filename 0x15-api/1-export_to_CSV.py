7e3f302a795aaca2380fdbdb9fe96374667cf291
smithjilks <smithjilks@gmail.com>
2022-02-01 04:01:17 +0300

    Add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that extends 0-gather_data_from_an_API.py
+to export data in the CSV format. """
+import csv
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user = requests.get(url + "/users/{}".format(user_id))
+    todos = requests.get(url + '/todos')
+    name = user.json().get('username')
+    file_name = sys.argv[1] + '.csv'
+
+    with open(file_name, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL,
+                            lineterminator='\n')
+        for todo in todos.json():
+            if todo.get('userId') == user_id:
+                writer.writerow([user_id, name, str(todo.get('completed')),
+                                todo.get('title')])
