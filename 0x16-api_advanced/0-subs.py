b52b5ca677a23689900462b57757d7d3fa7ae44a
smithjilks <smithjilks@gmail.com>
2022-02-16 01:48:30 +0300

    Add 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This script queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ queries the Reddit API and returns the number of subscribers"""
+
+    res = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit))
+    if res.status_code >= 300:
+        return 0
+
+    return res.json().get("data").get("subscribers")
+
+
+if __name__ == '__main__':
+    print(number_of_subscribers('programming'))
