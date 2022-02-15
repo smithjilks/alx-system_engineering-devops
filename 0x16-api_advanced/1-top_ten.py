198a35eb4507a5982f67a9fc02e41f4cbd56ee30
smithjilks <smithjilks@gmail.com>
2022-02-16 02:05:18 +0300

    Add 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This script contains a function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """ queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+
+    res = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                       .format(subreddit))
+    if res.status_code >= 300:
+        print('None')
+    else:
+        for child in res.json().get("data").get("children"):
+            print(child.get('data').get('title'))
