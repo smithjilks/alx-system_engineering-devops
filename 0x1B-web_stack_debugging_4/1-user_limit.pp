44fdcba023ad4230e90dba7ab6da7ccfe8873c8a
smithjilks <smithjilks@gmail.com>
2022-02-25 00:57:36 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 #This script increases number of worker_processes
 exec { 'increase nginx worker processes':
-  command => "sed -i 's/soft    nofile  .*/soft    nofile  65535/g' /etc/security/limits.conf;
-sed -i 's/hard    nofile  .*/hard    nofile  65535/g' /etc/security/limits.conf;",
+  command => "sed -i '$ i/* soft    nofile  1000' /etc/security/limits.conf;
+sed -i '$ i/* hard    nofile  100000' /etc/security/limits.conf;",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   }
