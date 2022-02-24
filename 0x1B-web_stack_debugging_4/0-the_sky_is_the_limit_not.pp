abfc91bb57b2e2f60b013c29055d7c6ca9a5840f
smithjilks <smithjilks@gmail.com>
2022-02-24 23:48:22 +0300

    Update 0-the_sky_is_the_limit_not

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #This script increases number of worker_processes
 exec { 'increase nginx worker processes':
-  command => "sed -i 's/worker_processes auto;/worker_processes 5;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  command => "sed -i 's/worker_processes 5;/worker_processes 8;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   }
